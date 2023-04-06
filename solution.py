329b1054c3432b3caa60d88e95222d50bd82eaac
ArtRakh <114469896+ArtRakh@users.noreply.github.com>
2023-04-06 23:39:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,5 +8,5 @@ chat_id = 1369690762 # Ваш chat ID, не меняйте название пе
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    return (x.max() - .014)/alpha**(1/len(x)) + .014, \
-           x.max()
+    return [x.max(), \
+           (x.max() - .014)/alpha**(1/len(x)) + .014]
